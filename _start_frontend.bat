02b42c526bc80028960bb910f80753310580e893
aadidevj007 <aadidevj4@gmail.com>
2026-04-08 22:38:10 +0530

    Refresh README and update ignore rules

--- _start_frontend.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title TraffixAI Frontend
-cd /d "%~dp0frontend"
-if not exist node_modules (
-    echo Installing npm packages...
-    call npm install --legacy-peer-deps
-)
-npm run dev
-pause
